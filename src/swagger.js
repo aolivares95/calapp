15be3fdbcf476157fd3a7bd8300d0b17f944a2ab
aolivares95 <antonio.olivares1995@gmail.com>
2025-12-17 23:23:56 -0600

    swagger doc

---
+++ src/swagger.js
@@ -0,0 +1,17 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'My Calapp API',
+    description: 'modify calendar events and save to DB'
+  },
+  host: 'https://192.168.1.251'
+};
+
+const outputFile = './swagger-output.json';
+const routes = ['./calevent.routes.js'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen()(outputFile, routes, doc);
